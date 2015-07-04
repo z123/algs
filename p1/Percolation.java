447efcfd986b4030864fd2c57efa3e075f2c3afb
z <z@zs-MacBook-Pro.local>
2015-07-04 15:01:45 -0700

    finished

--- p1/Percolation.java
+++ p1/Percolation.java
@@ -2,15 +2,17 @@ public class Percolation {
     // variables
     int[][] grid;
     WeightedQuickUnionUF wquf;
+    int N;
 
 
     // create N-by-N grid, with all sites blocked
     public Percolation(int N) {
+        this.N = N;
         this.grid = new int[N][N];
         this.wquf = new WeightedQuickUnionUF(N*N + 2);
         for (int i = 1; i <= N; i++) {
-            this.wquf.union(0,i);
-            this.wquf.union(N*N+1,i*N);
+            this.wquf.union(i,0);
+            this.wquf.union((N-1)*N+i, N*N+1);
         }
     }
 
@@ -21,23 +23,17 @@ public class Percolation {
         boolean left = j != 0;
         boolean right = j != this.grid.length - 1;
         boolean bottom = i != this.grid.length - 1;
-        int newi = i+1;
-        int newj = j+1;
         if (top && this.isOpen(i-1,j)) {
-            System.out.println("TOP");
-            this.wquf.union((newi-1)*newj, newi*newj);
+            this.wquf.union(this.N*i+j+1, this.N*(i-1)+j+1);
         }
         if (left && this.isOpen(i,j-1)) {
-            System.out.println("LEFT");
-            this.wquf.union(newi*(newj-1), newi*newj);
+            this.wquf.union(this.N*i+j+1, this.N*i+(j-1)+1);
         }
         if (right && this.isOpen(i,j+1)) {
-            System.out.println("RIGHT");
-            this.wquf.union((newi-1)*newj, newi*newj);
+            this.wquf.union(this.N*i+j+1, this.N*i+(j+1)+1);
         }
         if (bottom && this.isOpen(i+1,j)) {
-            System.out.println("BOT");
-            this.wquf.union((newi-1)*newj, newi*newj);
+            this.wquf.union(this.N*i+j+1, this.N*(i+1)+j+1);
         }
     }
 
@@ -50,23 +46,22 @@ public class Percolation {
     public boolean isFull(int i, int j) {
         i++;
         j++;
-        return this.wquf.connected(i * j, 0);
+        return this.wquf.connected(0, i * j);
     }
 
     // does the system percolate?
     public boolean percolates() {
-        return this.wquf.connected(0, this.grid.length + 1);
+        return this.wquf.connected(0,N*N+1);
     }
 
     // test client (optional)
     public static void main(String[] args) {
-
         Percolation p = new Percolation(5);
         p.open(0,0);
         p.open(1,0);
         p.open(2,0);
-        p.open(3,0);
-        p.open(4,0);
+        p.open(3, 0);
+        p.open(4, 0);
         System.out.println(p.percolates());
     }
 }
--- p1/PercolationStats.java
+++ p1/PercolationStats.java
@@ -1,42 +1,52 @@
 public class PercolationStats {
     // Variables
-    int[] results;
+    double[] results;
 
     // Perform T independent experiments on an N-by-N grid
     public PercolationStats(int N, int T) {
 
-        this.results = new int[T];
+        this.results = new double[T];
         for (int i = 0; i < T; i++) {
             Percolation p = new Percolation(N);
+            double count=0;
+            while(!p.percolates()) {
+                int x = StdRandom.uniform(N);
+                int y = StdRandom.uniform(N);
+                if (!p.isOpen(x,y)) {
+                    p.open(x,y);
+                    count++;
+                }
+            }
+            this.results[i] = count / (N*N);
         }
 
     }
 
     // sample mean of percolation threshold
     public double mean() {
-
-        return 0;
+        return StdStats.mean(this.results);
     }
 
     // sample standard deviation of percolation threshold
     public double stddev() {
-
-        return 0;
+        return StdStats.stddev(this.results);
     }
 
     // low  endpoint of 95% confidence interval
     public double confidenceLo() {
-
-        return 0;
+        return this.mean() - 1.96*this.stddev() / Math.sqrt(this.results.length);
     }
 
     // high endpoint of 95% confidence interval
     public double confidenceHi() {
-        return 0.0;
+        return this.mean() + 1.96*this.stddev() / Math.sqrt(this.results.length);
     }
 
     // test client (described below)
     public static void main(String[] args) {
-
+        PercolationStats ps = new PercolationStats(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+        StdOut.printf("mean                    = %f\n", ps.mean());
+        StdOut.printf("stdev =                 = %f\n", ps.stddev());
+        StdOut.printf("95%% confidence interval = %f, %f\n", ps.confidenceLo(), ps.confidenceHi());
     }
 }
